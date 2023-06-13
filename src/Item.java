17b2320cc1599015fb4d1781b86ab960767e8170
Vladimir <vladimirtang@gmail.com>
2023-06-13 16:29:54 +0800

    Add documention for Item and Slot class

--- src/Item.java
+++ src/Item.java
@@ -1,4 +1,14 @@
 
+/**
+ * This class represents the object of an item in a vending machine.
+ * The attribute name represents the name of the Item.
+ * The attribute calories represents the amount of calories contained in an Item.
+ * The attribute calories represents the cost of the Item.
+ * The attribute individual represents if the item can be bought individually or not
+ * 
+ * @author Vladimir Tang
+ *
+ */
 public class Item {
 	
 	private String name;
@@ -6,6 +16,14 @@ public class Item {
 	private int price;
 	private boolean individual;
 	
+	/**
+	 * This constructor method takes in the necessary information that constitute an Item.
+	 * 
+	 * @param name is the name of the Item
+	 * @param calories is the Item's amount of calories
+	 * @param price is the cost of the Item
+	 * @param individual True means that it cannot be bought individually
+	 */
 	public Item(String name, int calories, int price, boolean individual)
 	{
 		this.name = name;
@@ -14,6 +32,12 @@ public class Item {
 		this.individual = individual;
 	}
 	
+	/**
+	 * This constructor method takes in an Item object as a parameter.
+	 * It technically does the same thing as the other constructor method.
+	 * 
+	 * @param item is an Item instance
+	 */
 	public Item(Item item)
 	{
 		this.setName(item.getName());
@@ -22,41 +46,81 @@ public class Item {
 		this.setIndividual(item.getIndividual());
 	}
 	
+	/**
+	 * Sets the attribute name.
+	 * 
+	 * @param name will be the new String value of the attribute name
+	 */
 	public void setName(String name)
 	{
 		this.name = name;
 	}
 	
+	/**
+	 * Sets the attribute calories to a specific value.
+	 * 
+	 * @param calories will be the new value of the attribute calories
+	 */
 	public void setCalories(int calories)
 	{
 		this.calories = calories;
 	}
 	
+	/**
+	 * Sets the attribute price to a specific value.
+	 * 
+	 * @param price will be the new value of the attribute price
+	 */
 	public void setPrice(int price)
 	{
 		this.price = price;
 	}
 	
+	/**
+	 * Sets the attribute individual to either True or False.
+	 * 
+	 * @param individual will be the new value of the attribute individual
+	 */
 	public void setIndividual(boolean individual)
 	{
 		this.individual = individual;
 	}
 	
+	/**
+	 * Returns the attribute string name.
+	 * 
+	 * @return the string name attribute
+	 */
 	public String getName()
 	{
 		return name;
 	}
 	
+	/**
+	 * Returns the attribute calories.
+	 * 
+	 * @return the attribute calories
+	 */
 	public int getCalories()
 	{
 		return calories;
 	}
 	
+	/**
+	 * Returns the attribute price.
+	 * 
+	 * @return the attribute price
+	 */
 	public int getPrice()
 	{
 		return price;
 	}
 	
+	/**
+	 * Returns the attribute individual,
+	 * 
+	 * @return the attribute individual
+	 */
 	public boolean getIndividual()
 	{
 		return individual;
--- src/Slot.java
+++ src/Slot.java
@@ -1,11 +1,32 @@
 import java.util.LinkedList;
 
+/**
+ * This represents a slot in a vending machine.
+ * It inherits the attributes and methods of the class Item.
+ * It contains the variable availability as an attribute
+ * which represents the number of Item instance in a slot machine.
+ * The attribute sold represents the number of Item in a slot dispensed or used.
+ * As an attribute, the linked list of Items represents the instances of Item in a slot
+ * 
+ * @author Vladimir Tang
+ *
+ */
 public class Slot extends Item{
 
 	private int availability;
 	private int sold;
 	private LinkedList<Item> items;
 	
+	/**
+	 * This is constructor method which requires the following parameters
+	 * to copy all of its input to its corresponding attribute,
+	 * The attributes availability and sold are set to zero by default.
+	 * 
+	 * @param name is the name of the Item
+	 * @param calories is the Item's amount of calories
+	 * @param price is the cost of the Item
+	 * @param individual True means that it cannot be bought individually
+	 */
 	public Slot(String name, int calories, int price, boolean individual)
 	{
 		super(name, calories, price, individual);	
@@ -14,6 +35,12 @@ public class Slot extends Item{
 		sold = 0;
 	}
 	
+	/**
+	 * This constructor method takes in an Item object as a parameter.
+	 * It technically does the same thing as the other constructor method.
+	 * 
+	 * @param item is an Item instance
+	 */
 	public Slot(Item item)
 	{
 		super(item);
@@ -22,6 +49,11 @@ public class Slot extends Item{
 		sold = 0;
 	}
 	
+	/**
+	 * This method stocks a number of Item instance in a slot
+	 * 
+	 * @param num is the number of Item to added in the linked list of Item
+	 */
 	public void stock(int num)
 	{
 		for (int i = 0; i < num; i++)
@@ -31,37 +63,69 @@ public class Slot extends Item{
 		}
 	}
 	
+	/**
+	 * Remove the first Item in the linked list and reduce the availability by one.
+	 */
 	public void destroyItem()
 	{
 		items.removeFirst();
 		availability -= 1;
 	}
 	
+	/**
+	 * Sets the attribute availability to a specific value.
+	 * 
+	 * @param num will be the value of the attribute availability
+	 */
 	public void setAvailability(int num)
 	{
 		this.availability = num;
 	}
 	
+	/**
+	 * Sets the attribute sold to a specific value.
+	 * 
+	 * @param sold will be the value of the attribute sold
+	 */
 	public void setSold(int sold)
 	{
 		this.sold = sold;
 	}
 	
+	/**
+	 * Sets the attribute items.
+	 * @param items will be the linked list of the attribute items
+	 */
 	public void setItems(LinkedList<Item> items)
 	{
 		this.items = items;
 	}
 	
+	/**
+	 * Returns the attribute availability.
+	 * 
+	 * @return the attribute availability
+	 */
 	public int getAvailability()
 	{
 		return availability;
 	}
 	
+	/**
+	 * Returns the attribute sold.
+	 * 
+	 * @return the attribute sold
+	 */
 	public int getSold()
 	{
 		return sold;
 	}
 	
+	/**
+	 * Returns the attribute linked list items.
+	 * 
+	 * @return returns the attribute linked list items
+	 */
 	public LinkedList<Item> getItems()
 	{
 		return items;
